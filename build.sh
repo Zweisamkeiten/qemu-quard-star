6cdc15704c40b89de3e85397172bc6e786831e12
Einsam <snitxmhm@gmail.com>
2023-02-22 21:26:26 +0800

    ch4: 制作测试固件验证串口打印

--- build.sh
+++ build.sh
@@ -8,3 +8,23 @@ fi
 
 make -j"$(nproc)"
 make install
+
+CROSS_PREFIX=/usr/bin/riscv64-linux-gnu
+if [ ! -d "$BUILD_FOLDER/output/lowlevelboot" ]; then  
+  mkdir -p "$BUILD_FOLDER"/output/lowlevelboot
+fi  
+
+cd lowlevelboot || exit
+
+$CROSS_PREFIX-gcc -x assembler-with-cpp -c startup.s -o "$BUILD_FOLDER"/output/lowlevelboot/startup.o
+$CROSS_PREFIX-gcc -nostartfiles -T./boot.lds -Wl,-Map="$BUILD_FOLDER"/output/lowlevelboot/lowlevel_fw.map -Wl,--gc-sections "$BUILD_FOLDER"/output/lowlevelboot/startup.o -o "$BUILD_FOLDER"/output/lowlevelboot/lowlevel_fw.elf
+$CROSS_PREFIX-objcopy -O binary -S "$BUILD_FOLDER"/output/lowlevelboot/lowlevel_fw.elf "$BUILD_FOLDER"/output/lowlevelboot/lowlevel_fw.bin
+$CROSS_PREFIX-objdump --source --demangle --disassemble --reloc --wide "$BUILD_FOLDER"/output/lowlevelboot/lowlevel_fw.elf > "$BUILD_FOLDER"/output/lowlevelboot/lowlevel_fw.lst
+
+cd "$BUILD_FOLDER"/output/lowlevelboot || exit
+
+rm -rf fw.bin
+dd of=fw.bin bs=1k count=64k if=/dev/zero
+dd of=fw.bin bs=1k conv=notrunc seek=0 if=lowlevel_fw.bin
+
+cd "$BUILD_FOLDER" || exit
