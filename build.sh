e016df4420a2989e5e00ca17daf34bef5638c6b5
Einsam <snitxmhm@gmail.com>
2023-02-23 20:01:28 +0800

    ch5: add opensbi quard_star support

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+MAKEJOBS=$(nproc)
+CROSS_PREFIX=riscv64-linux-gnu-
 BUILD_FOLDER=$(cd "$(dirname "$0")" || exit;pwd)
 
 if [[ ! -d "$BUILD_FOLDER/output/qemu" ]]; then
   ./configure --prefix="$BUILD_FOLDER"/output/qemu  --target-list=riscv64-softmmu --enable-gtk  --enable-virtfs --disable-gio --enable-debug
 fi
 
-make -j"$(nproc)"
+make -j"$MAKEJOBS"
 make install
 
 CROSS_PREFIX=/usr/bin/riscv64-linux-gnu
@@ -23,8 +25,24 @@ $CROSS_PREFIX-objdump --source --demangle --disassemble --reloc --wide "$BUILD_F
 
 cd "$BUILD_FOLDER"/output/lowlevelboot || exit
 
+# 编译 OpenSBI
+if [ ! -d "$BUILD_FOLDER/output/opensbi" ]; then
+  mkdir -p "$BUILD_FOLDER/output/opensbi"
+fi
+
+cd "$BUILD_FOLDER"/opensbi || exit
+make -j"$MAKEJOBS" CROSS_COMPILE="$CROSS_PREFIX" PLATFORM=quard_star
+cp -r "$BUILD_FOLDER/opensbi/build/platform/quard_star/firmware"/*.bin "$BUILD_FOLDER/output/opensbi/"
+
+# 合成 firmware 固件
+if [ ! -d "$BUILD_FOLDER/output/firmware" ]; then
+  mkdir -p "$BUILD_FOLDER/output/firmware"
+fi
+cd "$BUILD_FOLDER"/output/firmware || exit
+
 rm -rf fw.bin
 dd of=fw.bin bs=1k count=64k if=/dev/zero
-dd of=fw.bin bs=1k conv=notrunc seek=0 if=lowlevel_fw.bin
+dd of=fw.bin bs=1k conv=notrunc seek=0 if="$BUILD_FOLDER"/output/lowlevelboot/lowlevel_fw.bin
+dd of=fw.bin bs=1k conv=notrunc seek=2k if="$BUILD_FOLDER"/output/opensbi/fw_jump.bin
 
 cd "$BUILD_FOLDER" || exit
--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ SHELL_FOLDER=$(cd "$(dirname "$0")" || exit; pwd)
   -M quard-star                                     \
   -m 1G                                             \
   -smp 2                                            \
-  -drive if=pflash,bus=0,unit=0,format=raw,file="$SHELL_FOLDER"/output/lowlevelboot/fw.bin \
+  -drive if=pflash,bus=0,unit=0,format=raw,file="$SHELL_FOLDER"/output/firmware/fw.bin \
   -nographic --parallel none
 
